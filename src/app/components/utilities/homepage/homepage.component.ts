3e5c7d2fec15f83440155d8536d0e982286402d0
Juan Esteban Muñoz <78231207+JuanMudi@users.noreply.github.com>
2024-05-28 11:44:03 -0500

    Header correction

--- src/app/components/utilities/homepage/homepage.component.ts
+++ src/app/components/utilities/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../../../services/header.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+  
+    constructor(private headerService: HeaderService) { }
+  
+    ngOnInit(): void {
+      if (sessionStorage.getItem('token')) {
+        this.headerService.setHeaderState(true);
+      }
+      else {
+        this.headerService.setHeaderState(false);
+      }
+    }
+
 
 }
